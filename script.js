af1fbffbf4b20cdf368330c3c63928a42fc15653
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-10 03:54:38 +0600

    promise

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// asynchronous and synchronous
+// promise
+// const getData = new Promise((resolve, reject) => {
+//   const num = Math.random() * 10;
+//   console.log("generated num:", num);
+//   if (num > 5) {
+//     resolve({ num: num });
+//   } else {
+//     reject({ err: "DATA is not available" });
+//   }
+// });
+// getData.then((data) => console.log(data)).catch((error) => console.log(error));
+
+//promise All
