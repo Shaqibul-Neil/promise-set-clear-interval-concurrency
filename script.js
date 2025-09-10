efa37873c3dc3cc2932edef9d2d94a8bf3d70ab2
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-10 12:31:46 +0600

    promise all

--- script.js
+++ script.js
@@ -54,3 +54,34 @@
 // Promise.all([getData1, getData2, getData3])
 //   .then((response) => console.log(response))
 //   .catch((error) => console.log(error));
+
+// const getData = new Promise((resolve, reject) => {
+//   const num = Math.random() * 10;
+//   console.log(num);
+//   //calling resolve and reject function send it as obj to get the value
+//   if (num > 3) resolve({ status: "resolved", value: num });
+//   else reject({ status: "rejected", value: num });
+// });
+// //consuming a promise with then catch and finally.
+// getData
+//   .then((data) => console.log("✅", data))
+//   .catch((error) => console.log("❌", error))
+//   .finally(() => console.log("Promise finished"));
+
+// const getData2 = new Promise((resolve, reject) => {
+//   const num = Math.random() * 10;
+//   console.log(num);
+//   //calling resolve and reject function send it as obj to get the value
+//   if (num > 3) resolve({ status: "resolved2", value: num });
+//   else reject({ status: "rejected2", value: num });
+// });
+// //consuming a promise with then catch and finally.
+// getData
+//   .then((data) => console.log("✅", data))
+//   .catch((error) => console.log("❌", error))
+//   .finally(() => console.log("Promise2 finished"));
+
+// Promise.all([getData, getData2])
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log("Promise done"));
